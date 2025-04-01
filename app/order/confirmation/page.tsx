6e4f41cd34f90a883491fea292bbe4da13017230
jee-in <chris309804@gmail.com>
2025-04-01 18:32:14 +0900

    feat: Add order confirmation page

---
+++ app/order/confirmation/page.tsx
@@ -0,0 +1,13 @@
+const ReservationConfirmationPage = () => {
+
+  return (
+      <div className="mx-auto p-5 md:p-10 pt-5 overflow-y-auto ">
+        <p className="text-center text-xl md:text-2xl font-bold mb-2">
+          {"예매가 완료되었습니다."}
+        </p>
+        {/* <ReservationOverviewTemplate data={paymentData} /> */}
+      </div>
+  );
+};
+
+export default ReservationConfirmationPage;
